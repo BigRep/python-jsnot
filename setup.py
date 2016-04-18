56566891de7d48b287c8d12ab4ff5607dc69e980
Yan Foto <yan.foto@quaintous.com>
2016-04-18 16:48:54 +0200

    Add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="python-jsnot",
+      version="0.0.1",
+      description="Convenient module to ease the use of JSON objects.",
+      author="Yan Foto",
+      author_email="yan.foto@quaintous.com",
+      packages=["bigrep"]
+      )
